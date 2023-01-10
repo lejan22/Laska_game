c8d8d1d9da54eaf4dc9a36ea1f2ffee806e3982a
lejan22 <alejandrodl2206@gmail.com>
2023-01-10 14:04:21 +0100

    player movement

---
+++ Assets/Scripts/Player_controller.cs
@@ -0,0 +1,159 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_controller : MonoBehaviour
+{
+    public int lives = 3;
+    public float speed = 30f;
+    public float maxspeed = 30f;
+    public float rotationSpeed;
+    public float jumpForce = 2f;
+    public float diveForce = 12f;
+    public float stompForce = 15f;
+
+    private float horizontalInput, verticalInput;
+    private bool isJumping;
+
+    public float jumpCutMultiplier = 9.8f;
+    private int lastJumpTime;
+    private bool jumpInputReleased;
+    private bool canDive;
+
+    private Rigidbody playerRigidbody;
+
+    public LayerMask groundLayerMask;
+
+    private float groundDistance = 1.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRigidbody = GetComponent<Rigidbody>();
+        lives = 3;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Jumping();
+
+        Diving();
+
+        ActionStomp();
+    }
+    private void FixedUpdate()
+    {
+        
+       horizontalInput = Input.GetAxis("Horizontal");
+       verticalInput = Input.GetAxis("Vertical");
+
+       playerRigidbody.AddForce(Vector3.forward * speed * verticalInput, ForceMode.Force);
+       playerRigidbody.AddForce(Vector3.right * horizontalInput * speed);
+        
+        // Velocidad maxima del rigidbody del player
+        if (playerRigidbody.velocity.magnitude > maxspeed)
+        {
+            // Mantiene a la velocidad maxima
+            playerRigidbody.velocity = playerRigidbody.velocity.normalized * maxspeed;
+        }
+        
+        Stop();
+       
+    }
+    //Salta
+    private void Jumping()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && IsOnGround())
+        {
+            playerRigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            
+
+            isJumping = true;
+
+            canDive = true;
+            
+        }
+    }
+    public void OnJumpUp()
+    {
+        if(playerRigidbody.velocity.y > 0 && isJumping)
+        {
+            playerRigidbody.AddForce(Vector3.down * playerRigidbody.velocity.y * (1 - jumpCutMultiplier), ForceMode.Impulse);
+            jumpInputReleased = true;
+            lastJumpTime = 0;
+        }
+    }
+    //Se lanza hacia adelante
+    private void Diving()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && !IsOnGround()&& canDive)
+        {
+            
+            playerRigidbody.AddForce(Vector3.up * 5, ForceMode.Impulse);
+            playerRigidbody.AddForce(Vector3.forward * diveForce, ForceMode.Impulse);
+
+            canDive = false;
+        }
+
+        
+
+    }
+    private void ActionStomp()
+    {
+        // Si presionas el click izquierdo del raton
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            // Aplica una fuerza, un impulso vertical hacia abajo
+            playerRigidbody.AddForce(Vector3.down * stompForce, ForceMode.Impulse);
+
+          
+        }
+    }
+    private void Stop()
+    {
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            playerRigidbody.velocity =(Vector3.zero);
+        }
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            playerRigidbody.velocity =(Vector3.zero);
+        }
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            playerRigidbody.velocity= (Vector3.zero);
+        }
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            playerRigidbody.velocity = (Vector3.zero);
+        }
+    }
+    private void characterRotation()
+    {
+        Vector3 movementDirection = new Vector3(horizontalInput, 0, verticalInput);
+
+        movementDirection.Normalize();
+
+        transform.Translate(movementDirection * speed * Time.deltaTime
+, Space.World);
+
+        if (movementDirection != Vector3.zero)
+        {
+            Quaternion toRotation = Quaternion.LookRotation(movementDirection, Vector3.up);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
+        }
+    }
+    private bool IsOnGround()
+    {
+        // Raycast hacia abajo con una distancia determinada
+        Physics.Raycast(transform.position, Vector3.down, out RaycastHit hitData, groundDistance, groundLayerMask);
+
+        // Devuelve cualquier bool diferente a NULL
+        return hitData.collider != null;
+
+       
+    }
+
+
+}
---
+++ Assets/Scripts/Player_shadow.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_shadow : MonoBehaviour
+{
+
+    public GameObject player;
+
+    private Vector3 hitDataPoint;
+    private float groundDistance;
+
+    void Update()
+    {
+        // Raycast en direccion hacia abajo
+        if (Physics.Raycast(player.transform.position, Vector3.down, out RaycastHit hitData, 50))
+        {
+            // Guarda el hit del RayCast
+            hitDataPoint = hitData.point;
+
+            // Modifica el axis vertical de este Vector3
+            hitDataPoint.y += 0.1f;
+
+            // Sigue la posicion del hit
+            transform.position = hitDataPoint;
+
+            // Toma la altura entre la sombra y el player
+            groundDistance = transform.position.y - player.transform.position.y;
+
+            // Escala la sombra segun la distancia con el suelo
+            transform.localScale = Vector3.one * -groundDistance;
+        }
+    }
+}
