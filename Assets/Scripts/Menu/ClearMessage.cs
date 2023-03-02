8a5f7ab82ceb5fdbe44e1bb8ecb99321a13cd4a4
lejan22 <91455326+lejan22@users.noreply.github.com>
2023-03-02 22:40:55 +0100

    advances

---
+++ Assets/Scripts/Menu/ClearMessage.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClearMessage : MonoBehaviour
+{
+
+    public AudioSource _audioS;
+    public AudioClip[] rankvoiceClips;
+    public AudioClip Celebrationsong;
+    public Animator _animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _audioS = GetComponent<AudioSource>();
+
+        StartCoroutine(Clearmessage());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator Clearmessage()
+    {
+
+        _animator.Play("Clear_idle");
+
+        yield return new WaitForSeconds(2);
+
+        
+        _animator.Play("Clear");
+        Debug.Log("Henlo");
+        int randomIndex = Random.Range(0, rankvoiceClips.Length);
+        _audioS.PlayOneShot(rankvoiceClips[randomIndex], 1);
+        _audioS.PlayOneShot(Celebrationsong, 1);
+    }
+}
--- Assets/Scripts/Menu/Result_screen.cs
+++ Assets/Scripts/Menu/Result_screen.cs
@@ -9,26 +9,35 @@ public class Result_screen : MonoBehaviour
 
     public AudioSource _audioS;
     public AudioClip[] rankvoiceClips;
+    public AudioClip Celebrationsong;
     private int Roes;
 
     public TextMeshProUGUI collectedRoes;
     public TextMeshProUGUI TotalTime;
     public TextMeshProUGUI Rank;
 
+    public Animator _animator;
+
     // Start is called before the first frame update
     void Start()
     {
         Cursor.lockState = CursorLockMode.None;
         _audioS = GetComponent<AudioSource>();
+       
     }
-
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
     // Update is called once per frame
     void Update()
     {
         
     }
 
-    public void rank()
+    
+
+    /*public void rank()
     {
         if (Roes == 100)
         {
@@ -37,5 +46,5 @@ public class Result_screen : MonoBehaviour
             _audioS.PlayOneShot(rankvoiceClips[randomIndex], 1);
         }
         
-    }
+    }*/
 }
